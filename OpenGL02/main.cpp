39d9a038293c319033abc7c79fcc31fad2dadc8d
Sasha Shundrikova <tim.bread@yandex.ru>
2017-11-23 18:06:11 +0300

    added light and rotations

--- OpenGL02/main.cpp
+++ OpenGL02/main.cpp
@@ -7,47 +7,87 @@ double rotate_x = 0;
 double rotate_y = 0;
 double rotate_z = 0;
 
+double pedestal_rotate_x = 0;
+double pedestal_rotate_y = 0;
+double pedestal_rotate_z = 0;
+
 static int spin = 0;
 
 void Init(void) {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	//glEnable(GL_LIGHT1);
+	glEnable(GL_NORMALIZE);
 	glEnable(GL_DEPTH_TEST);
+}
+
+void Enable_Light0(void) {
+	glEnable(GL_LIGHT0);
+
+	GLfloat position[] = { -2.0, 0.0, 2.0, 1.0 };
+	GLfloat ambientLight[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat diffuseLight[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat specularLight[] = { 1.0, 1.0, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+}
+
+void Enable_Light1(void) {
+	glEnable(GL_LIGHT1);
+
+	GLfloat position[] = { 2.5, 0.0, -2.5, 1.0 };
+	GLfloat ambientLight[] = { 0.5, 0.0, 0.0, 1.0 };
+	GLfloat diffuseLight[] = { 1.0, 0.5, 1.0, 1.0 };
+	GLfloat specularLight[] = { 1.0, 1.0, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT1, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, specularLight);
+	glLightfv(GL_LIGHT1, GL_POSITION, position);
+}
+
+void Enable_Light2(void) {
+	glEnable(GL_LIGHT2);
+
+	GLfloat position[] = { 2.0, 2.0, 3.0, 1.0 };
+	//{ 0.8, 0.8, 0.2, 1.0 };
+	GLfloat spot_direction[] = { 0.0, 0.0, 1.0 };
+
+	GLfloat ambientLight[] = { 0.5, 0.3, 0.3, 1.0 };
+	GLfloat diffuseLight[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat specularLight[] = { 1.0, 1.0, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT2, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, specularLight);
+	glLightfv(GL_LIGHT2, GL_POSITION, position);
+
+	//glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 45.0);
+	glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, spot_direction);
+	glLightf(GL_LIGHT2, GL_SPOT_EXPONENT, 15.0);
+
 }
 
 void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glPushMatrix();
+	// light 0
+	Enable_Light0();
+
+	// light 1
+	Enable_Light1();
+
+	// light 2
+	Enable_Light2();
 
 	glPushMatrix();
-	GLfloat position1[] = { 0.0, 0.0, 1.5, 1.0};
-	glLightfv(GL_LIGHT0, GL_POSITION, position1);
-	//glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.0);
-	//glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.2);
-	//glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.4);
 
-	/*GLfloat ambientLight[] = { 0.2, 0.2, 0.2, 1.0 };
-	GLfloat diffuseLight[] = { 0.2, 0.2, 0.2, 1.0 };
-	GLfloat specularLight[] = { 10, 10, 10, 10 };
-
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);*/
-
-	glPopMatrix();
-
-	//glPushMatrix();
-	//GLfloat position2[] = { 0.0, 0.0, 0.0, 1.0 };
-	////glRotated(170.0, 1.0, 1.0, 1.0);
-	//glLightfv(GL_LIGHT1, GL_POSITION, position2);
-	////glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 0.0);
-	////glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.2);
-	////glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.4);*/
-	//glPopMatrix();
+	glRotatef(pedestal_rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(pedestal_rotate_y, 0.0, 1.0, 0.0);
+	glRotatef(pedestal_rotate_z, 0.0, 0.0, 1.0);
 
 	// bronze cube
 	glPushMatrix();
@@ -60,8 +100,13 @@ void Display(void) {
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_bronze);
 	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 25.6);
 
-	glTranslatef(2.0, -1.0, 0.0);
-	glScalef(2.0, 1.0, 2.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+	glRotatef(rotate_z, 0.0, 0.0, 1.0);
+	//glTranslatef(2.0, -1.0, 0.0);
+	//glScalef(2.0, 1.0, 2.0);
+
 	glutSolidCube(1);
 	glPopMatrix();
 
@@ -76,7 +121,11 @@ void Display(void) {
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_golden);
 	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 51.2);
 
-	glScalef(2.0, 3.0, 2.0);
+	glTranslatef(0.0, 0.5, 0.0);
+	glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+	glRotatef(rotate_z, 0.0, 0.0, 1.0);
+	glScalef(1.0, 2.0, 1.0);
 
 	glutSolidCube(1);
 	glPopMatrix();
@@ -92,8 +141,12 @@ void Display(void) {
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_silver);
 	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 51.2);
 
-	glTranslatef(-2.0, -0.5, 0.0);
-	glScalef(2.0, 2.0, 2.0);
+	glTranslatef(-1.0, 0.0, 0.0);
+	glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+	glRotatef(rotate_z, 0.0, 0.0, 1.0);
+	//glTranslatef(-2.0, -0.5, 0.0);
+	//glScalef(2.0, 2.0, 2.0);
 
 	glutSolidCube(1);
 	glPopMatrix();
@@ -107,15 +160,46 @@ void Display(void) {
 void Reshape(int width, int height) {
 	w = width;   h = height;
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-	//gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.5, 20.0);
+	//glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 20.0);
+	gluPerspective(45, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(2.0, 3.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(2.0, 3.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
+void Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+		// вокруг центра сцены		
+		// все вокруг центра пьедестала почета
+		case 'w': pedestal_rotate_x -= 5; break;
+		case 's': pedestal_rotate_x += 5; break;
+		case 'a': pedestal_rotate_y += 5; break;
+		case 'd': pedestal_rotate_y -= 5; break;
+		case 'q': pedestal_rotate_z += 5; break;
+		case 'e': pedestal_rotate_z -= 5; break;
+	}
+	glutPostRedisplay();
+}
+
+
+void specialKeys(int key, int x, int y) {
+
+	switch (key) {
+		// вокруг своей оси
+		case GLUT_KEY_UP: rotate_x += 5; break;
+		case GLUT_KEY_DOWN: rotate_x -= 5; break;
+		case GLUT_KEY_RIGHT: rotate_y += 5; break;
+		case GLUT_KEY_LEFT: rotate_y -= 5; break;
+		case GLUT_KEY_PAGE_UP: rotate_z += 5; break;
+		case GLUT_KEY_PAGE_DOWN: rotate_z -= 5; break;
+	}
+
+	glutPostRedisplay();
+}
 
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
@@ -126,6 +210,8 @@ int main(int argc, char* argv[]) {
 	Init();
 	glutReshapeFunc(Reshape);
 	glutDisplayFunc(Display);
+	glutSpecialFunc(specialKeys);
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 	return 0;
 }
